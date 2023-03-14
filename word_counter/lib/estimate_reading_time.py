9e1a322fd851748848c77c9a5032a7c3577af273
James <dougyturnbull@gmail.com>
2023-03-14 13:19:35 +0000

    .. 6

--- word_counter/lib/estimate_reading_time.py
+++ word_counter/lib/estimate_reading_time.py
@@ -1,4 +1,6 @@
 def estimate_reading_time(text):
+    if not text:
+        raise Exception('ERROR - no text entered')
     words = text.split()
     word_count = len(words)
     return word_count / 200
