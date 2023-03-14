787b1481407d99095fe0da2adad08f82188226ea
James <dougyturnbull@gmail.com>
2023-03-14 12:48:16 +0000

    .. 5

--- word_counter/lib/grammar_stats.py
+++ word_counter/lib/grammar_stats.py
@@ -2,24 +2,37 @@ class GrammarStats:
     def __init__(self):
         self.num_checked = 0
         self.num_passed = 0
+    
     def check(self, text):
+        if not text:
+            return False
         self.num_checked += 1
-        if text[-1] == ('.' or '!' or '?') and (text[0].isupper()):
+        if text[-1] in ('.', '!', '?') and (text[0].isupper()):
             self.num_passed += 1
             return True
         else:
             return False
-        # Parameters:
-        #   text: string
-        # Returns:
-        #   bool: true if the text begins with a capital letter and ends with a
-        #         sentence-ending punctuation mark, false otherwise
+# Parameters:
+#   text: string
+# Returns:
+#   bool: true if the text begins with a capital letter and ends with a
+#         sentence-ending punctuation mark, false otherwise
+
+
     def percentage_good(self):
         if self.num_checked == 0:
-            return self.num_passed == 0
+            return 0
         percent_pass = int((self.num_passed / self.num_checked) * 100)
         return percent_pass
-        # Returns:
-        #   int: the percentage of texts checked so far that passed the check
-        #        defined in the `check` method. The number 55 represents 55%
+# Returns:
+#   int: the percentage of texts checked so far that passed the check
+#        defined in the `check` method. The number 55 represents 55%
 
+
+stats = GrammarStats()
+GrammarStats()
+
+text = ''
+result = stats.check(text)
+
+print(result)
--- word_counter/tests/test_GrammarStats.py
+++ word_counter/tests/test_GrammarStats.py
@@ -2,54 +2,65 @@ import pytest
 from lib.grammar_stats import GrammarStats
 
 
-# Given an empty string
-# Raises an error
-def test_empty_string_returns_error():
+def test_non_empty_string_returns_correct_result():
     test1 = GrammarStats()
-    result = test1.check()
+    result = test1.check("Text.")
     assert result == True
 
 
-# Given a string that ends with correct punctuation
-# Returns True
 def test_ends_with_punctuation():
     test2 = GrammarStats()
-    result = test2.check('text!')
+    result = test2.check('Text.')
     assert result == True
 
 
-# Given a string that starts with upper case and ends with correct punctuation
-# Returns True
-def test_starts_with_upper_and_ends_with_punctuation():
-    test2 = GrammarStats()
-    result = test2.check('Text!')
-    assert result == True
-
-
-# Given a string that starts with lower case
-# Returns False
 def test_starts_with_lower_case():
     test3 = GrammarStats()
     result = test3.check('text.')
     assert result == False
 
 
-# Given multiple tests
-# Will return the percentage that pass as True so far
-def test_percentage_true_so_far():
+def test_percentage_good_with_no_checked_texts():
     test4 = GrammarStats()
-    test4.check('Text!')
-    test4.check('text')
     result = test4.percentage_good()
-    assert result == 50
+    assert result == 0
 
 
-# Given multiple tests
-# Will return the percentage that pass as True so far
-def test_percentage_true_so_far_again():
+def test_percentage_good_with_all_passed_texts():
     test5 = GrammarStats()
-    test5.check('Text!')
-    test5.check('text')
-    test5.check('Hello.')
+    test5.check('Text.')
+    test5.check('Hello!')
     result = test5.percentage_good()
+    assert result == 100
+
+
+def test_percentage_good_with_some_passed_texts():
+    test6 = GrammarStats()
+    test6.check('Text.')
+    test6.check('text.')
+    test6.check('Hello!')
+    result = test6.percentage_good()
     assert result == 66
+
+
+def test_check_with_invalid_input():
+    test7 = GrammarStats()
+    result = test7.check('')
+    assert result == False
+
+def test_check_with_no_punctuation():
+    test8 = GrammarStats()
+    result = test8.check('Text')
+    assert result == False
+
+
+def test_check_with_all_upper_case():
+    test9 = GrammarStats()
+    result = test9.check('TEXT!')
+    assert result == True
+
+
+def test_check_with_multiple_sentences():
+    test10 = GrammarStats()
+    result = test10.check('Hello! This is a test.')
+    assert result == True
