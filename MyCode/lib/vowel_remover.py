3efc7a448134bea0077c5bfe12157b5db371011f
James <dougyturnbull@gmail.com>
2023-03-17 13:19:06 +0000

    vowel remover 2

--- MyCode/lib/vowel_remover.py
+++ MyCode/lib/vowel_remover.py
@@ -1,4 +1,4 @@
-class VowelRemover:
+class VowelRemoverDud:
     def __init__(self, text):
         self.text = text
         self.vowels = ["a", "e", "i", "o", "u"]
@@ -10,3 +10,32 @@ class VowelRemover:
                 self.text = self.text[:i] + self.text[i+1:]
             i += 1
         return self.text
+
+# this does not account for consecutive vowels in the string   
+# When the method encounters a vowel in the text, it removes that vowel from the string 
+# by slicing the string and re-assigning it to the original text variable. 
+#
+# However, it only removes the current vowel and moves to the next character, 
+# not considering if the next character is also a vowel.
+
+class VowelRemover:
+    def __init__(self, text):
+        self.text = text
+        self.vowels = ["a", "e", "i", "o", "u"]
+
+    def remove_vowels(self):
+        i = 0
+        while i < len(self.text):
+            if self.text[i].lower() in self.vowels:
+                self.text = self.text[:i] + self.text[i+1:]
+            else:
+                i += 1
+        return self.text
+    
+# In the updated implementation, the else block is executed when the character at the 
+# current index is not a vowel. In this block, [i] is incremented by 1, 
+# so the loop moves on to the next character regardless of whether it is a vowel or not. 
+# 
+# This means that if two or more vowels are consecutive in the input string, 
+# the loop will continue removing vowels until a consonant is encountered, 
+# since [i] is incremented only when the current character is not a vowel.
--- MyCode/tests/test_vowel_remover.py
+++ MyCode/tests/test_vowel_remover.py
@@ -1,5 +1,37 @@
 from lib.vowel_remover import *
 
+def test_simple_dud():
+    remover = VowelRemoverDud("ab")
+    result_no_vowels = remover.remove_vowels()
+    assert result_no_vowels == "b"
+
+def test_long_sentence_with_punctuation_dud():
+    remover = VowelRemoverDud("We will remove the vowels from this sentence.")
+    result_no_vowels = remover.remove_vowels()
+    assert result_no_vowels == "W wll rmv th vwls frm ths sntnc."
+
+# def test_remove_every_vowel_at_once_dud():
+#     remover = VowelRemoverDud("aeiou")
+#     result_no_vowels = remover.remove_vowels()
+#     assert result_no_vowels == ""
+
+def test_remove_every_vowel_at_once_dud_forced():
+    remover = VowelRemoverDud("aeiou")
+    result_no_vowels = remover.remove_vowels()
+    assert result_no_vowels == "eo"
+
+# def test_remove_consecutive_vowels_dud():
+#     remover = VowelRemoverDud("Our Glorious Leader Pol Pot is in the pool")
+#     result_no_vowels = remover.remove_vowels()
+#     assert result_no_vowels == "r Glrs Ldr Pl Pt s n th pl"
+
+def test_remove_consecutive_vowels_dud_forced():
+    remover = VowelRemoverDud("Our Glorious Leader Pol Pot is in the pool")
+    result_no_vowels = remover.remove_vowels()
+    assert result_no_vowels == "ur Glros Ladr Pl Pt s n th pol"
+
+##########################################################################################    
+
 def test_simple():
     remover = VowelRemover("ab")
     result_no_vowels = remover.remove_vowels()
@@ -9,3 +41,13 @@ def test_long_sentence_with_punctuation():
     remover = VowelRemover("We will remove the vowels from this sentence.")
     result_no_vowels = remover.remove_vowels()
     assert result_no_vowels == "W wll rmv th vwls frm ths sntnc."
+
+def test_remove_every_vowel_at_once():
+    remover = VowelRemover("aeiou")
+    result_no_vowels = remover.remove_vowels()
+    assert result_no_vowels == ""
+
+def test_remove_consecutive_vowels():
+    remover = VowelRemover("Our Glorious Leader Pol Pot is in the pool")
+    result_no_vowels = remover.remove_vowels()
+    assert result_no_vowels == "r Glrs Ldr Pl Pt s n th pl"
